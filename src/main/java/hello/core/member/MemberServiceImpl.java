5c757f3d6994c811c8f8e9d4e894ccf9d57f2302
Nakhoon choi <cnh6123@naver.com>
2022-08-17 13:59:44 +0900

    [UPDATE] 테스트 코드 간단하게 작성 및 실행 App 임시 생성

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
+  //new 생성자를 통해 실제 객체를 의존한다는 것이 문제이다.(DIP 위반)
   private final MemberRepository memberRepository = new MemoryMemberRepository();
 
   @Override
