676289f8758d304f5ee624d9ceb1495ab0f5321d
Nakhoon choi <cnh6123@naver.com>
2022-08-24 17:12:09 +0900

    [UPDATE] jpa repository test 코드 작성 및 build.gradle 의존성 추가

---
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,31 @@
+package study.datajpa.repository;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.NoSuchElementException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+@SpringBootTest
+@Transactional //같은 트랜잭션에서 영속성 컨텍스트가 같은 인스턴스임을 보장한다.
+@Rollback(false)
+class MemberRepositoryTest {
+
+  @Autowired MemberRepository memberRepository;
+
+  @Test
+  public void testMember(){
+    Member member = new Member("memberA");
+    Member savedMember = memberRepository.save(member);
+
+    Member findMember = memberRepository.findById(savedMember.getId()).orElseThrow();
+
+    assertThat(findMember.getId()).isEqualTo(member.getId());
+    assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    assertThat(findMember).isEqualTo(member);
+  }
+}
