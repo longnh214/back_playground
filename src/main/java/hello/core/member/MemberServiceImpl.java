b9ec0e99819513906c984241d1ef8bb9a2557dac
Nakhoon choi <cnh6123@naver.com>
2022-08-25 10:00:00 +0900

    [UPDATE] DIP를 적용하고 그에 맞게 App 실행 코드 수정

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,7 +3,11 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService{
 
   //new 생성자를 통해 실제 객체를 의존한다는 것이 문제이다.(DIP 위반)
-  private final MemberRepository memberRepository = new MemoryMemberRepository();
+  private final MemberRepository memberRepository;
+
+  public MemberServiceImpl(MemberRepository memberRepository){
+    this.memberRepository = memberRepository;
+  }
 
   @Override
   public void join(Member member) {
