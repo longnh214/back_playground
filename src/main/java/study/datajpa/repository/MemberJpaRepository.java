9d74b8839e186b36115bee996db658bec71f4200
Nakhoon choi <cnh6123@naver.com>
2022-08-25 13:58:48 +0900

    [UPDATE] 멤버와 팀에 대한 jpa 레포지토리 작성

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -1,5 +1,7 @@
 package study.datajpa.repository;
 
+import java.util.List;
+import java.util.Optional;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +18,26 @@ public class MemberJpaRepository {
   public Member save(Member member){
     em.persist(member);
     return member;
+  }
+
+  public void delete(Member member){
+    em.remove(member);
+  }
+
+  public List<Member> findAll(){
+    //JPQL(jpa가 제공하는 쿼리 문법)
+    return em.createQuery("select m from Member m", Member.class)
+        .getResultList();
+  }
+
+  public Optional<Member> findById(Long id){
+    Member member  = em.find(Member.class, id);
+    return Optional.ofNullable(member); // Java 8 기본 (공부 할 것)
+  }
+
+  public long count(){
+    return em.createQuery("select count(m) from Member m", Long.class)
+        .getSingleResult();
   }
 
   public Member find(Long id){
