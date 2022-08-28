37becf91aac345be9fdb0fc0ffac0c7ce9b50f26
Nakhoon choi <cnh6123@naver.com>
2022-08-28 10:00:00 +0900

    [UPDATE] 정액할인정책에서 정률할인정책으로 변경

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,9 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -14,7 +17,7 @@ public class AppConfig {
     return new MemberServiceImpl(memberRepository());
   }
 
-  private MemoryMemberRepository memberRepository() {
+  private MemberRepository memberRepository() {
     return new MemoryMemberRepository();
   }
 
@@ -22,7 +25,7 @@ public class AppConfig {
     return new OrderServiceImpl(memberRepository(), discountPolicy());
   }
 
-  private FixDiscountPolicy discountPolicy() {
-    return new FixDiscountPolicy();
+  private DiscountPolicy discountPolicy() {
+    return new RateDiscountPolicy();
   }
 }
