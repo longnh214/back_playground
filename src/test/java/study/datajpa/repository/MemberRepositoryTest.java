7122ad8df1351b40d483be019c799ade6af5e2c3
Nakhoon choi <cnh6123@naver.com>
2022-08-31 10:00:00 +0900

    [UPDATE] spring data jpa를 이용한 member 테스트 코드 작성

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -2,6 +2,7 @@ package study.datajpa.repository;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,5 +28,35 @@ class MemberRepositoryTest {
     assertThat(findMember.getId()).isEqualTo(member.getId());
     assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
     assertThat(findMember).isEqualTo(member);
+  }
+
+  @Test
+  public void basicCRUD(){
+    Member member1 = new Member("member1");
+    Member member2 = new Member("member2");
+    memberRepository.save(member1);
+    memberRepository.save(member2);
+
+    //단 건 조회 검증
+    Member findMember1 = memberRepository.findById(member1.getId()).orElseThrow();
+    Member findMember2 = memberRepository.findById(member2.getId()).orElseThrow();
+
+    assertThat(findMember1).isEqualTo(member1);
+    assertThat(findMember2).isEqualTo(member2);
+
+    //리스트 조회 검증
+    List<Member> all = memberRepository.findAll();
+    assertThat(all.size()).isEqualTo(2);
+
+    //count 검증
+    long count = memberRepository.count();
+    assertThat(count).isEqualTo(2);
+
+    //삭제 검증
+    memberRepository.delete(member1);
+    memberRepository.delete(member2);
+
+    long deletedCount = memberRepository.count();
+    assertThat(deletedCount).isEqualTo(0);
   }
 }
