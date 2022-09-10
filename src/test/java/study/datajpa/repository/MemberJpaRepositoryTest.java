57ab4fbef1d772369d9dd63e3754672824ea383f
Nakhoon choi <cnh6123@naver.com>
2022-09-10 10:00:00 +0900

    [UPDATE] jpa paging 조회 메소드 적용 및 테스트 작성

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -87,4 +87,29 @@ class MemberJpaRepositoryTest {
 
     assertThat(findMember).isEqualTo(member1);
   }
+  
+  @Test
+  public void paging(){
+    //given
+    memberJpaRepository.save(new Member("member1", 10));
+    memberJpaRepository.save(new Member("member2", 10));
+    memberJpaRepository.save(new Member("member3", 10));
+    memberJpaRepository.save(new Member("member4", 10));
+    memberJpaRepository.save(new Member("member5", 10));
+
+    int age = 10;
+    int offset = 0;
+    int limit = 3;
+
+    //when
+    List<Member> findByPageList = memberJpaRepository.findByPage(age, offset, limit);
+    long totalCount = memberJpaRepository.totalCount(age);
+
+    //페이지 계산 공식 적용...
+    //totalPage = totalCount / size ...
+
+    //then
+    assertThat(findByPageList.size()).isEqualTo(3);
+    assertThat(totalCount).isEqualTo(5);
+  }
 }
