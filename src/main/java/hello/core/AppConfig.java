1b9f1b33ca3c5bf0d6149170db8b588c7015af52
Nakhoon choi <cnh6123@naver.com>
2023-01-16 16:58:35 +0900

    [UPDATE] 스프링 일부 적용

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,23 +9,30 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 //객체의 생성과 연결을 담당하는 class
+@Configuration //설정 정보
 public class AppConfig {
   //생성자 주입
+  @Bean
   public MemberService memberService(){
     return new MemberServiceImpl(memberRepository());
   }
 
-  private MemberRepository memberRepository() {
+  @Bean
+  public MemberRepository memberRepository() {
     return new MemoryMemberRepository();
   }
 
+  @Bean
   public OrderService orderService(){
     return new OrderServiceImpl(memberRepository(), discountPolicy());
   }
 
-  private DiscountPolicy discountPolicy() {
+  @Bean
+  public DiscountPolicy discountPolicy() {
     return new RateDiscountPolicy();
   }
 }
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,12 +1,17 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
   private final MemberRepository memberRepository;
+//  private final MemberRepository memberRepository = new MemoryMemberRepository();
   private final DiscountPolicy discountPolicy; //Null Pointer Exception 발생
+//  private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//  private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
   public OrderServiceImpl(MemberRepository memberRepository,
       DiscountPolicy discountPolicy) {
