6af4bd0bb7ea17a4ff79453081a7d8b782ce5193
Nakhoon choi <cnh6123@naver.com>
2023-04-03 11:27:47 +0900

    [UPDATE] 3장 템플릿 메서드 익명 내부 클래스 방식 구현 코드 작성

--- src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -23,6 +23,29 @@ public class TemplateMethodTest {
     template2.execute();
   }
 
+  @Test
+  void templateMethodV2(){
+    AbstractTemplate template1 = new AbstractTemplate(){
+
+      @Override
+      protected void call() {
+        log.info("비즈니스 로직 1 실행");
+      }
+    };
+    log.info("클래스 이름 1 = {}", template1.getClass());
+    template1.execute();
+
+    AbstractTemplate template2 = new AbstractTemplate(){
+
+      @Override
+      protected void call() {
+        log.info("비즈니스 로직 2 실행");
+      }
+    };
+    log.info("클래스 이름 2 = {}", template2.getClass());
+    template2.execute();
+  }
+
   private void logic1(){
     long startTime = System.currentTimeMillis();
     //비즈니스 로직 실행
