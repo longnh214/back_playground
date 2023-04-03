247a80c46c338e718e5953335d7c31124428e927
Nakhoon choi <cnh6123@naver.com>
2023-04-03 15:49:54 +0900

    [UPDATE] 3장 람다와 익명 클래스를 이용한 전략 패턴 코드 작성

--- src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -38,6 +38,9 @@ public class ContextV1Test {
     log.info("resultTime={}", resultTime);
   }
 
+  /**
+   * 전략 패턴 사용
+   */
   @Test
   void strategyV1(){
     Strategy strategyLogic1 = new StrategyLogic1();
@@ -46,6 +49,60 @@ public class ContextV1Test {
 
     Strategy strategyLogic2 = new StrategyLogic2();
     ContextV1 context2 = new ContextV1(strategyLogic2);
+    context2.execute();
+  }
+
+  @Test
+  void strategyV2(){
+    Strategy strategyLogic1 = new Strategy(){
+
+      @Override
+      public void call() {
+        log.info("비즈니스 로직 1 실행");
+      }
+    };
+    ContextV1 context1 = new ContextV1(strategyLogic1);
+    log.info("strategyLogic1={}", strategyLogic1.getClass());
+    context1.execute();
+
+    Strategy strategyLogic2 = new Strategy(){
+
+      @Override
+      public void call() {
+        log.info("비즈니스 로직 2 실행");
+      }
+    };
+    ContextV1 context2 = new ContextV1(strategyLogic2);
+    log.info("strategyLogic2={}", strategyLogic2.getClass());
+    context2.execute();
+  }
+
+  @Test
+  void strategyV3(){
+    ContextV1 context1 = new ContextV1(new Strategy(){
+      @Override
+      public void call() {
+        log.info("비즈니스 로직 1 실행");
+      }
+    });
+    context1.execute();
+
+    ContextV1 context2 = new ContextV1(new Strategy(){
+
+      @Override
+      public void call() {
+        log.info("비즈니스 로직 2 실행");
+      }
+    });
+    context2.execute();
+  }
+
+  @Test
+  void strategyV4(){
+    ContextV1 context1 = new ContextV1(() -> log.info("비즈니스 로직 1 실행"));
+    context1.execute();
+
+    ContextV1 context2 = new ContextV1(() -> log.info("비즈니스 로직 2 실행"));
     context2.execute();
   }
 }
