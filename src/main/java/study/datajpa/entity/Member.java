de3bb2cb13eb557e76f8ca1b02bd853663229dc2
Nakhoon <cnh6123@naver.com>
2024-11-28 11:17:15 +0900

    [UPDATE] Named Lock 테스트 Service 코드 작성

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,21 +1,19 @@
 package study.datajpa.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.NamedQuery;
+import org.hibernate.annotations.Parameter;
 
 @Entity
-@Getter @Setter
+@Getter
+@Setter
 @ToString(of = {"id", "username", "age"})
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @NamedQuery(
@@ -23,7 +21,25 @@ import org.hibernate.annotations.NamedQuery;
     query="select m from Member m where m.username = :username"
 )
 public class Member {
-  @Id @GeneratedValue
+  @Id
+//  @GenericGenerator(
+//          name = "MEMBER_SEQ_GENERATOR",
+//          strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+//          parameters = {
+//                  @Parameter(name = "sequence_name", value = "MEMBER_SECOND_SEQ"),
+//                  @Parameter(name = "optimizer", value = "pooled"),
+//                  @Parameter(name = "initial_value", value = "1"),
+//                  @Parameter(name = "increment_size", value = "500")
+//          }
+//  )
+  @SequenceGenerator(
+          name = "MEMBER_SEQ_GENERATOR",   // 식별자 생성기 이름
+          sequenceName = "MEMBER_SEQ",  // 데이터베이스에 등록되어있는 시퀀스 이름: DB에는 해당 이름으로 매핑된다.
+          initialValue = 1,  // DDL 생성시에만 사용되며 시퀀스 DDL을 생성할 때 처음 시작하는 수를 지정
+          allocationSize = 50  // 시퀀스 한 번 호출에 증가하는 수
+  )
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
+//  @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "member_id")
   private Long id;
   private String username;
--- src/main/java/study/datajpa/service/SampleEntityService.java
+++ src/main/java/study/datajpa/service/SampleEntityService.java
@@ -2,6 +2,7 @@ package study.datajpa.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.SampleEntity;
 import study.datajpa.repository.SampleEntityRepository;
@@ -22,6 +23,14 @@ public class SampleEntityService {
     @Transactional
     public void updatePessimisticSampleEntity(Long id) throws InterruptedException{
         SampleEntity sampleEntity = sampleEntityRepository.findByIdWithPessimisticLock(id);
+        sampleEntity.decrease(1L);
+
+        sampleEntityRepository.saveAndFlush(sampleEntity);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void decrease(Long id){
+        SampleEntity sampleEntity = sampleEntityRepository.findById(id).orElseThrow(() -> new RuntimeException("Entity not found"));
         sampleEntity.decrease(1L);
 
         sampleEntityRepository.saveAndFlush(sampleEntity);
