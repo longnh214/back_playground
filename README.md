06e5410c30df5649edad8bd2c9aa273c667f33d2
Nakhoon <cnh6123@naver.com>
2025-04-19 15:28:38 +0900

    [UPDATE] .gitignore 적용

--- README.md
+++
@@ -1,1 +0,0 @@
-# Welcome to Nakhoon's backend playground. 
--- spring-security-example/src/main/java/com/spring/spring_security/common/OAuth2UserInfo.java
+++
@@ -1,7 +0,0 @@
-package com.spring.spring_security.common;
-
-public interface OAuth2UserInfo {
-    String getProvider();
-    String getProviderId();
-    String getUserName();
-}
